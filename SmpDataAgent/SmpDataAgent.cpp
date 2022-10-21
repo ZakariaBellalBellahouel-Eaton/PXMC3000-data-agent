5f5ee5ec67ee22a73cafd6765865b656c73493e9
Zakaria Bellal Bellahouel <ZakariaBellalBellahouel@eaton.com>
2022-10-21 11:45:21 -0400

    Change device IP Adresse

--- SmpDataAgent/SmpDataAgent.cpp
+++ SmpDataAgent/SmpDataAgent.cpp
@@ -36,7 +36,7 @@ int main()
 	// Refresh Time
 	const int refreshTime = 2;
 	// SMP IP adress
-	const string stringSmpBasicUri = "https://10.106.88.181/";
+	const string stringSmpBasicUri = "https://10.130.129.17/";
 	// TO by encrypted in param file
 	const string username = "security";
 	const string password = "Security8*";
